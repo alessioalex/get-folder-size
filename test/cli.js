8caec63ee3927625f44b8f5ea32596ba8a0d19b3
Anton <dev@atjn.dk>
2024-07-20 02:40:38 +0200

    Skip CLI test on OS with bad separator

--- test/cli.js
+++ test/cli.js
@@ -17,40 +17,44 @@ const exec = promisify(execCallback);
  */
 const dirname = pathDirname(fileURLToPath(import.meta.url));
 
+/**
+ * Skip the test on operating systems with non-standard path separators (Windows).
+ */
+const skip = sep === "/";
+
 const cwd = joinPaths(dirname, "..");
 const sizeOfFixtureFolder = "0.01 MB";
 
 for (const folderArg of ["--folder=", "-f=", ""]) {
-	const args = `${folderArg}test${sep}fixture`;
+	const args = `${folderArg}test/fixture`;
 
 	tap.test(`get folder size with args: ${args}`, async () => {
-		const result = await exec(`bin${sep}get-folder-size.js ${args}`, {
+		const result = await exec(`bin/get-folder-size.js ${args}`, {
 			cwd,
 		});
 
 		tap.ok(
 			result.stdout.startsWith(sizeOfFixtureFolder),
 			"should return the size of the folder",
+			{ skip },
 		);
 	});
 
 	for (const ignoreArg of ["--ignore=", "-i="]) {
 		for (const flipArgs of [false, true]) {
-			const arg1 = `${folderArg}test${sep}fixture`;
+			const arg1 = `${folderArg}test/fixture`;
 			const arg2 = `${ignoreArg}.*txt`;
 			const args = flipArgs ? `${arg2} ${arg1}` : `${arg1} ${arg2}`;
 
 			tap.test(`get folder size with args: ${args}`, async () => {
-				const result = await exec(
-					`bin${sep}get-folder-size.js ${args}`,
-					{
-						cwd,
-					},
-				);
+				const result = await exec(`bin/get-folder-size.js ${args}`, {
+					cwd,
+				});
 
 				tap.ok(
 					result.stdout.startsWith("0.00 MB"),
 					"should return zero as size",
+					{ skip },
 				);
 			});
 		}
@@ -58,7 +62,11 @@ for (const folderArg of ["--folder=", "-f=", ""]) {
 }
 
 tap.test("get folder size with missing args", async () => {
-	tap.rejects(async () => {
-		await exec(`bin${sep}get-folder-size.js`, { cwd });
-	}, "should reject since no folder path is provided");
+	tap.rejects(
+		async () => {
+			await exec("bin/get-folder-size.js", { cwd });
+		},
+		"should reject since no folder path is provided",
+		{ skip },
+	);
 });
