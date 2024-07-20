feaea73a3763aea84df34f46f89e455ffdc6ca1a
Anton <dev@atjn.dk>
2024-07-20 21:36:18 +0200

    Refactor CLI tests

--- test/cli.js
+++ test/cli.js
@@ -1,5 +1,5 @@
 import { exec as execCallback } from "node:child_process";
-import { join as joinPaths, dirname as pathDirname, sep } from "node:path";
+import { join as joinPaths, dirname as pathDirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { promisify } from "node:util";
 import tap from "tap";
@@ -9,6 +9,8 @@ const exec = promisify(execCallback);
 /**
  * @file
  * This test checks that basic CLI functionality works.
+ * Most of the args parsing is handled by a node module, so we don't
+ * need to test a million edge cases, they should be handled correctly.
  */
 
 /**
@@ -18,60 +20,55 @@ const exec = promisify(execCallback);
 const dirname = pathDirname(fileURLToPath(import.meta.url));
 
 /**
- * Skip the test on operating systems with non-standard path separators (Windows).
+ * Skip the test on Windows. It has some weird args parsing that we don't want to deal with.
+ * If the tests pass on all other platforms, we can expect it to work correctly on Windows as well.
  */
-const skip = sep !== "/";
+const skip = process.platform === "win32";
 
 const cwd = joinPaths(dirname, "..");
 const sizeOfFixtureFolder = "0.01 MB";
 
-for (const folderArg of ["--folder=", "-f=", ""]) {
-	const args = `${folderArg}test/fixture`;
+if (!skip) {
+	for (const folderArg of ["--folder ", "--folder=", "-f ", ""]) {
+		const args = `${folderArg}"test/fixture"`;
 
-	tap.test(`get folder size with args: ${args}`, async () => {
-		if (skip) return;
+		tap.test(`get folder size with args: ${args}`, async () => {
+			const result = await exec(`bin/get-folder-size.js ${args}`, {
+				cwd,
+			});
 
-		const result = await exec(`bin/get-folder-size.js ${args}`, {
-			cwd,
+			tap.ok(
+				result.stdout.startsWith(sizeOfFixtureFolder),
+				"should return the size of the folder",
+			);
 		});
 
-		tap.ok(
-			result.stdout.startsWith(sizeOfFixtureFolder),
-			"should return the size of the folder",
-			{ skip },
-		);
-	});
+		for (const ignoreArg of ["--ignore ", "--ignore=", "-i "]) {
+			for (const flipArgs of [false, true]) {
+				const arg1 = `${folderArg}"test/fixture"`;
+				const arg2 = `${ignoreArg}".*txt"`;
+				const args = flipArgs ? `${arg2} ${arg1}` : `${arg1} ${arg2}`;
 
-	for (const ignoreArg of ["--ignore=", "-i="]) {
-		for (const flipArgs of [false, true]) {
-			const arg1 = `${folderArg}test/fixture`;
-			const arg2 = `${ignoreArg}.*txt`;
-			const args = flipArgs ? `${arg2} ${arg1}` : `${arg1} ${arg2}`;
+				tap.test(`get folder size with args: ${args}`, async () => {
+					const result = await exec(
+						`bin/get-folder-size.js ${args}`,
+						{
+							cwd,
+						},
+					);
 
-			tap.test(`get folder size with args: ${args}`, async () => {
-				if (skip) return;
-
-				const result = await exec(`bin/get-folder-size.js ${args}`, {
-					cwd,
+					tap.ok(
+						result.stdout.startsWith("0.00 MB"),
+						"should return zero as size",
+					);
 				});
-
-				tap.ok(
-					result.stdout.startsWith("0.00 MB"),
-					"should return zero as size",
-					{ skip },
-				);
-			});
+			}
 		}
 	}
-}
 
-tap.test("get folder size with missing args", async () => {
-	if (skip) return;
-	tap.rejects(
-		async () => {
+	tap.test("get folder size with missing args", async () => {
+		tap.rejects(async () => {
 			await exec("bin/get-folder-size.js", { cwd });
-		},
-		"should reject since no folder path is provided",
-		{ skip },
-	);
-});
+		}, "should reject since no folder path is provided");
+	});
+}
